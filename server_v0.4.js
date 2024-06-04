af2816070589ffa2750b09fb101aff1807c31983
geiriadur <139781329+geiriadur@users.noreply.github.com>
2024-06-04 18:18:02 +0100

    osgoi crasio os llinyn gwag

--- server_v0.4.js
+++ server_v0.4.js
@@ -537,10 +537,14 @@ http.createServer(async function (req, res) {
 	function acceptLang () {
 		var acceptLang;
 		acceptLang = req.headers["accept-language"];
-		acceptLangArray = acceptLang.split(','); // get first lang in sequence
-		acceptLang = acceptLangArray[0]; // put string back together
-		acceptLangArray = acceptLang.split("-"); // removes -gb, -us etc
-		acceptLang = acceptLangArray[0]; // put string back together
+		if (acceptLang != "") {
+			acceptLangArray = acceptLang.split(','); // get first lang in sequence
+			acceptLang = acceptLangArray[0]; // put string back together
+		}
+		if (acceptLang != "") {
+			acceptLangArray = acceptLang.split("-"); // removes -gb, -us etc
+			acceptLang = acceptLangArray[0]; // put string back together
+		}
 		return acceptLang;
 	}
 	
